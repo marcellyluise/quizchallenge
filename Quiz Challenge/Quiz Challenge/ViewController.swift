dfb2ae15e3bf4ba3c2af8dd000c5ecb8f9f4e605
Marcelly Luise Souza Godinho <marcelly.luise1@aisdigital.com.br>
2019-10-27 18:12:09 -0300

    First version of bottom view UX with keyboard interaction

---
+++ Quiz Challenge/Quiz Challenge/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Quiz Challenge
+//
+//  Created by Marcelly Luise on 27/10/19.
+//  Copyright © 2019 Celly Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        tableView.tableFooterView = UIView()
+    }
+
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            
+            UIView.animate(withDuration: 0.3) {
+                self.bottomConstraint.constant = keyboardSize.height + 100
+            }
+            
+            
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        
+        UIView.animate(withDuration: 0.3) {
+            self.bottomConstraint.constant = 0
+        }
+
+    }
+
+}
+
+extension ViewController: UITableViewDelegate {
+
+}
+
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+
+
