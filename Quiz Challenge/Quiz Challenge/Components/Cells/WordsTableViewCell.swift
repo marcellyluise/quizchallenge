b787f23c86fa23aa3afd5ef9413d5c0fae5919ab
Marcelly Luise Souza Godinho <marcelly.luise1@aisdigital.com.br>
2019-10-27 20:05:18 -0300

    Create cell component so we can list words

---
+++ Quiz Challenge/Quiz Challenge/Components/Cells/WordsTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  WordsTableViewCell.swift
+//  Quiz Challenge
+//
+//  Created by Marcelly Luise on 27/10/19.
+//  Copyright © 2019 Celly Corp. All rights reserved.
+//
+
+import UIKit
+
+class WordsTableViewCell: UITableViewCell {
+
+    @IBOutlet private weak var wordLabel: UILabel!
+    
+    var word: String? = nil {
+        didSet {
+            wordLabel.text = word
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
