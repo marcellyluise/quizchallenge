ff62e862c93e5d795261a4b4dfe2c6bdf47e4949
Marcelly Luise <c1310567@bb.com.br>
2019-10-30 18:48:59 -0300

    [Saving state] Begin to create a custom Activity Indicator View

---
+++ Quiz Challenge/Quiz Challenge/Components/Custom Views/Activity Indicator/QuizActivityIndicatorView.swift
@@ -0,0 +1,15 @@
+//
+//  QuizActivityIndicatorView.swift
+//  Quiz Challenge
+//
+//  Created by INDRA BRASIL SOLUCOES E SERVICOS TECNOLOGICOS on 30/10/19.
+//  Copyright © 2019 Celly Corp. All rights reserved.
+//
+
+import UIKit
+
+class QuizActivityIndicatorView: XibLoader {
+    
+    
+
+}
